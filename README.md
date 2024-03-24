c184bc36aff92261cdf0f209dfb4d2ee72b044d7
katagaki <68231035+katagaki@users.noreply.github.com>
2024-03-24 20:04:25 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# reposts-back
+
+This Cypress script will help you re-enable reposts for any follows you have on X (formerly Twitter). It is mainly for former users of Blindfold for Twitter and other similar tools that are now defunct due to the API pricing changes.
+
+
+# Prerequisites
+
+- Node.js | [Download](https://nodejs.org/en/download) | [brew install](https://formulae.brew.sh/formula/node)
+- NPM | [Guide](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+- Your X archive | [Guide](https://help.twitter.com/managing-your-account/how-to-download-your-x-archive)
+
+
+# Setting Up
+
+1. Copy the `cypress.env-sample.json` to `cypress.env.json`.
+2. Insert your own username, password, and OTP secret into the `cypress.env.json` file.
+    - You may need to set up a new Authenticator app to obtain the OTP secret.
+3. Download a copy of your X data archive, and copy the entire array of accounts from the `data/follows.js` file into the `follows` value of the `cypress.env.json` file.
+    - A single sample is provided, in the event you are unsure of what to copy.
+
+
+# Running Cypress
+
+1. Open a new terminal in the root of the project (containing the `cypress` folder, `cypress.config.js` file, and so on).
+2. Run `npm install` to install `cypress` and `cypress-otp`.
+3. Run `npx cypress open` to open Cypress.
+4. Select `E2E Testing`.
+5. Select `Start E2E Testing in Electron`.
---
+++ cypress/e2e/reposts-back.cy.js
@@ -0,0 +1,60 @@
+describe('reposts-back for X', () => {
+    it('Open X login page', () => {
+        var username = Cypress.env('username')
+        var password = Cypress.env('password')
+        var otpSecret = Cypress.env('otpSecret')
+        var follows = Cypress.env('follows')
+
+        // Authenticate
+        cy.visit('https://twitter.com', {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+            }
+        })
+
+        var btnSignIn = cy.get('span').contains('Sign in')
+        btnSignIn.click()
+
+        var txtUsername = cy.get('input')
+        txtUsername.click()
+        txtUsername.type(username)
+        var btnNext = cy.get('span').contains('Next')
+        btnNext.click()
+
+        var txtPassword = cy.get('input[name="password"]')
+        txtPassword.click()
+        txtPassword.type(password)
+        var btnLogin = cy.get('span').contains('Log in')
+        btnLogin.click()
+
+        var txtOTP = cy.get('input')
+        txtOTP.click()
+        cy.task('generateOTP', otpSecret).then(token => {
+            txtOTP.type(token)
+        })
+        btnNext = cy.get('span').contains('Next')
+        btnNext.click()
+
+        // For each profile, re-enable reposts if they are off
+        for (follow in follows) {
+            var accountId = follow.following.accountId
+            var userLink = follow.following.userLink
+
+            cy.visit(userLink, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+                }
+            })
+
+            var btnUserActionMenu = cy.get('div[data-testid="userActions"]')
+            btnUserActionMenu.click()
+
+            var mnuUserActions = cy.get('div[data-testid="Dropdown"]')
+            var btnTurnRepostsOnOrOff = mnuUserActions.children('div[role="menuitem"]').first()
+            if (btnTurnRepostsOnOrOff.text().includes('Turn on Reposts')) {
+                btnTurnRepostsOnOrOff.click()
+                cy.get('div[role="alert"]')
+            }
+        }
+    })
+})
